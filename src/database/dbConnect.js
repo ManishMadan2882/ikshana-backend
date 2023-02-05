860e6f431755fd69e7692ecd79824c531aa3d4a2
Manish Madan <96079232+ManishMadan2882@users.noreply.github.com>
2023-02-05 15:25:09 +0530

    Update dbConnect.js

--- src/database/dbConnect.js
+++ src/database/dbConnect.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import dotenv from 'dotenv'
 dotenv.config();
 mongoose.set("strictQuery",false);
-const mongourl="mongodb://localhost:27017/healthapp";
+const mongourl="mongodb+srv://manishdev:XMbl7sjCYkse5tNC@cluster0.fbb5bhh.mongodb.net/test";
 export default mongoose.connect(process.env.MONGO_LOCAL)
 .then(()=>{
     console.log("MongoDB Connection: Success");
