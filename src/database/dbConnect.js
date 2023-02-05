922332f3cb53c697d5858f294a527892538b6bea
Manish Madan <96079232+ManishMadan2882@users.noreply.github.com>
2023-02-05 15:39:42 +0530

    Update dbConnect.js

--- src/database/dbConnect.js
+++ src/database/dbConnect.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv'
 dotenv.config();
 mongoose.set("strictQuery",false);
 const mongourl="mongodb+srv://manishdev:XMbl7sjCYkse5tNC@cluster0.fbb5bhh.mongodb.net/test";
-export default mongoose.connect(process.env.MONGO_LOCAL)
+export default mongoose.connect("mongodb+srv://manishdev:loging@cluster0.fbb5bhh.mongodb.net/test")
 .then(()=>{
     console.log("MongoDB Connection: Success");
 })
