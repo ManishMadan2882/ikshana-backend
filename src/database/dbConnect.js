2b05cdee81d029261c81ae016a41be53be79215b
Manish Madan <96079232+ManishMadan2882@users.noreply.github.com>
2023-02-09 14:11:14 +0530

    Add files via upload

--- src/database/dbConnect.js
+++ src/database/dbConnect.js
@@ -1,9 +1,10 @@
-import mongoose from "mongoose";
+import mongoose, { mongo } from "mongoose";
 import dotenv from 'dotenv'
 dotenv.config();
 mongoose.set("strictQuery",false);
-const mongourl="mongodb+srv://manishdev:XMbl7sjCYkse5tNC@cluster0.fbb5bhh.mongodb.net/test";
-export default mongoose.connect("mongodb+srv://manishdev:loging@cluster0.fbb5bhh.mongodb.net/test")
+//const mongourl="mongodb://localhost:27017/healthapp";
+
+export default mongoose.connect(process.env.MONGO)
 .then(()=>{
     console.log("MongoDB Connection: Success");
 })
